28ba70bee8baaf96d98e9a7e1bf1e9380bbcd05f
Miguel Mendez <miguel.mendez@novateva.com>
2025-08-01 17:32:58 -0400

    DB PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,8 +86,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "booking_db"),
+        "USER": os.getenv("POSTGRES_USER", "booking_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "booking_pass"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
